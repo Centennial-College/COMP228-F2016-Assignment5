d68998fd2a0faad3b1a9c9065642077503739a68
Kevin Ma <kbma.kevin@gmail.com>
2016-12-08 19:05:53 -0500

    v0.4.3 implemented event handlers for selecting table rows and editing textfields

--- src/exercise1/GameView.java
+++ src/exercise1/GameView.java
@@ -11,7 +11,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
  * @file OnlineGameTrackerView.java
  * @author Kevin Ma | #: 300867968
  * @date December 8, 2016
- * @version 0.4.2 added viewing functionality to the GameView table
+ * @version 0.4.3 implemented event handlers for selecting table rows and editing textfields
  * @description This class defines the structure and behaviors of the Game view
  *              for this application at a micro level.
  */
@@ -36,6 +36,7 @@ public class GameView extends OnlineGameTrackerView {
 	// update/delete
 	// ---------------------------------------------------------------------------------------------
 	private TextField gameTitleModifyTF;
+	private TextField gameIdModifyTF;
 	private Button updateBtn;
 	private Button deleteBtn;
 
@@ -60,10 +61,12 @@ public class GameView extends OnlineGameTrackerView {
 		super.start();
 		this.initializeControls();
 		this.addContentsToContainers();
-		this.resetTab();
+		this.addEventListeners();
 
 		// populate the table
 		this.table.setItems(gc.selectAll());
+
+		this.resetTab();
 	}
 
 	/**
@@ -85,9 +88,17 @@ public class GameView extends OnlineGameTrackerView {
 		this.updateOrDeleteTitledPane.setExpanded(true);
 		this.addTitledPane.setExpanded(false);
 
+		// disable buttons by default
+		this.addBtn.setDisable(true);
+		this.deleteBtn.setDisable(true);
+		this.updateBtn.setDisable(true);
+
 		// clear all textfields
 		this.gameTitleInputTF.clear();
 		this.gameTitleModifyTF.clear();
+
+		// cannot edit a non-referenced record
+		this.gameTitleModifyTF.setDisable(true);
 
 		// clear all labels
 		this.updateOrDeleteMsgLabel.setText("");
@@ -101,6 +112,82 @@ public class GameView extends OnlineGameTrackerView {
 	// PRIVATE METHODS
 	// =============================================================================================
 	/**
+	 * Define behaviors of the controls on the view
+	 */
+	private void addEventListeners() {
+		// TableView Event Handlers - when selecting a different record
+		this.table.getSelectionModel().selectedItemProperty().addListener(e -> {
+			// enable buttons to interact with selected record
+			this.updateBtn.setDisable(false);
+			this.deleteBtn.setDisable(false);
+			this.gameTitleModifyTF.setDisable(false);
+
+			// clear and hide previous message when selecting new record
+			this.updateOrDeleteMsgLabel.setText("");
+			this.updateOrDeleteMsgLblHBox.setManaged(false);
+
+			// populate textfields with data from selected table row
+			GameModel tmpGame = this.table.getSelectionModel().getSelectedItem();
+			this.gameIdModifyTF.setText(tmpGame.getGameId() + "");
+			this.gameTitleModifyTF.setText(tmpGame.getGameTitle());
+		});
+		// -----------------------------------------------------------------------------------------
+
+		// TextField Event Handlers
+		// only allows adding to the table if text is not null
+		this.gameTitleInputTF.textProperty().addListener(e -> {
+			if (this.gameTitleInputTF.getText().length() > 0)
+				this.addBtn.setDisable(false);
+			else
+				this.addBtn.setDisable(true);
+
+		});
+		// only allows updating/deleting from the table if text is not null
+		this.gameTitleModifyTF.textProperty().addListener(e -> {
+			if (this.gameTitleModifyTF.getText().length() > 0) {
+				this.updateBtn.setDisable(false);
+				this.deleteBtn.setDisable(false);
+			} else {
+				this.updateBtn.setDisable(true);
+				this.deleteBtn.setDisable(true);
+			}
+		});
+		// -----------------------------------------------------------------------------------------
+
+		// Button Event Handlers
+		this.addBtn.setOnAction(e -> {
+			this.addMsgLabel.setText("Successfully added '" + this.gameTitleInputTF.getText() + "' to the Game table.");
+			this.addMsgLblHBox.setManaged(true);
+			// prevents redoing same action
+			this.gameTitleInputTF.setText("");
+			this.addBtn.setDisable(true);
+		});
+		this.deleteBtn.setOnAction(e -> {
+			this.updateOrDeleteMsgLabel.setText("Successfully deleted game #" + this.gameIdModifyTF.getText() + " '"
+					+ this.gameTitleModifyTF.getText() + "' from the Game table.");
+			this.updateOrDeleteMsgLblHBox.setManaged(true);
+			// prevents redoing same action
+			this.gameTitleModifyTF.setText("");
+			this.gameIdModifyTF.setText("");
+			this.gameTitleModifyTF.setDisable(true);
+			this.updateBtn.setDisable(true);
+			this.deleteBtn.setDisable(true);
+		});
+		this.updateBtn.setOnAction(e -> {
+			this.updateOrDeleteMsgLblHBox.setManaged(true);
+			this.updateOrDeleteMsgLabel
+					.setText(String.format("Successfully updated game #%s to '%s' in the Game table.",
+							this.gameIdModifyTF.getText(), this.gameTitleModifyTF.getText()));
+			// prevents redoing same action
+			this.gameIdModifyTF.setText("");
+			this.gameTitleModifyTF.setText("");
+			this.gameTitleModifyTF.setDisable(true);
+			this.updateBtn.setDisable(true);
+			this.deleteBtn.setDisable(true);
+		});
+	}
+
+	/**
 	 * Adding the initialized controls to their container(s)
 	 */
 	private void addContentsToContainers() {
@@ -110,10 +197,11 @@ public class GameView extends OnlineGameTrackerView {
 		this.tabBodyVBox.getChildren().add(0, this.table);
 
 		// update/delete box
-		this.updateOrDeleteControlsHBox.getChildren().addAll(this.gameTitleInputTF, this.updateBtn, this.deleteBtn);
+		this.updateOrDeleteControlsHBox.getChildren().addAll(this.gameIdModifyTF, this.gameTitleModifyTF,
+				this.updateBtn, this.deleteBtn);
 
 		// add box
-		this.addControlsHBox.getChildren().addAll(this.gameTitleModifyTF, this.addBtn);
+		this.addControlsHBox.getChildren().addAll(this.gameTitleInputTF, this.addBtn);
 	}
 
 	/**
@@ -138,6 +226,12 @@ public class GameView extends OnlineGameTrackerView {
 		this.gameTitleInputTF = new TextField();
 		this.gameTitleInputTF.setPromptText("Game Title");
 		this.gameTitleInputTF.setMinWidth(300);
+		// -----------------------------------------------------------------------------------------
+		this.gameIdModifyTF = new TextField();
+		this.gameIdModifyTF.setPromptText("Game Id");
+		this.gameIdModifyTF.setMinWidth(100);
+		this.gameIdModifyTF.setDisable(true); // cannot edit to maintain
+												// referential integrity
 		// -----------------------------------------------------------------------------------------
 		this.gameTitleModifyTF = new TextField();
 		this.gameTitleModifyTF.setPromptText("Game Title");
